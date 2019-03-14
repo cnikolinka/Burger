069b6b5664f2471426fc6f50131c78d78f738913
Nikolinka Cieloszczyk <cnikolinka@gmail.com>
2019-03-14 03:36:19 -0400

    Anthony's changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,36 +3,6 @@ var burger = require("../models/burger");
 
 var router = express.Router();
 
-const orm = {
-  selectAll: function (cb) {
-      connection.query("SELECT * FROM restaurant_burger", function (err, data) {
-          if (err) cb(err, null);
-          cb(null, data);
-      });
-  },
-  selectAllBy: function(condition, value, cb) {
-      const sqlQuery = `SELECT * FROM restaurant_burger WHERE ${condition } = ${value}`;
-      connection.query(sqlQuery, function (err, data) {
-          if (err) cb(err, null);
-          cb(null, data)
-      });
-  },
-  insertOne: function (burgerName, cb) {
-      const sqlQuery = `INSERT INTO restaurant_burger(burger_name) VALUES('${burgerName}')`;
-      connection.query(sqlQuery, function (err, data) {
-          if (err) cb(err, null);
-          cb(null, data);
-      });
-  },
-
-  updateOne: function (condition, id, cb) {
-      const sqlQuery = `UPDATE restaurant_burger SET is_favorite = ${condition} WHERE id = ${id}`;
-      connection.query(sqlQuery, function (err, data) {
-          if (err) cb(err, null);
-          cb(null, data)
-      });
-  },
-
 router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var HandleBrsObj = {
